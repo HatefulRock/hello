77bc5984c0608bb0b8737e88a1611ac395749d80
HatefulRock <pierre.blachere@outlook.com>
2021-06-02 10:57:03 +0200

    Ex5

---
+++ Ex5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int print_tableau(int t[], int n){
+    int i=0;
+    while(i<n+1){
+        printf("t=%d\n",t[i]);
+        i++;
+    }
+
+}
+
+int pairs(){
+    int t[100];
+    int a=0;
+    for(int i=0;i<100;i++){
+        if (i%2==0){
+            t[a]=i;
+            a++;
+
+        }
+    }
+int print_tableau(t,a);
+}
